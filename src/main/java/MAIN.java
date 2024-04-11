457afd3f5ffbac76656f1cd4561d41e04d3df686
Sabil667 <msabi@myuax.com>
2024-04-11 11:33:28 +0200

    .

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,2 +1,13 @@
+import HerramientaAnalisisNumerico.SumatoriaListadoNumeros;
+
 public class MAIN {
+    public static void main(String[] args) {
+        // Prueba de la función sumatoria
+        int resultadoSumatoria = SumatoriaListadoNumeros.sumatoria(5);
+        System.out.println("El resultado de la sumatoria es: " + resultadoSumatoria);
+
+        // Prueba de la función listarNumeros
+        System.out.println("Listado de números:");
+        SumatoriaListadoNumeros.listarNumeros(1, 5);
+    }
 }
